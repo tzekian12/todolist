3e9615f155fa8eb8479d10cc13b64b731c2a38b5
issuser <issuser@ISS-CR2905.mshome.net>
2018-03-16 14:12:43 +0800

    Initial repo

---
+++ src/app/TodoListApp.java
@@ -0,0 +1,10 @@
+package app;
+
+public class TodoListApp {
+    @SuppressWarnings("unused")
+	public static void main ( String [] arguments )
+    {
+        System.out.println("Hello, world");
+        String input = System.console().readLine();
+    }
+}
